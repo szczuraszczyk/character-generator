f34012b40a2646e98928d6fa646fd0c57eba7907
szczuraszczyk <160737700+szczuraszczyk@users.noreply.github.com>
2025-10-17 08:06:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function createCharacter(imie, klasa) {
+
+    return {
+        name: imie,
+        role: klasa,
+        level: 1,
+        equipment: {
+            weapon: "miecz",
+            armor: "złote gacie",
+            items: ["garnek", "nadgryziony owad"]
+        }
+    }
+}
+
+function addItem(character, item) {
+    
+}
+
+const myCharacter = l
+
+console.log(myCharacter);
