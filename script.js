230f0581479c85682d17d6ed38e3db81d844536c
szczuraszczyk <ameli.szczelaszczyk@gmail.com>
2025-10-17 09:01:28 +0200

    piardu

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createCharacter(imie, klasa) {
         name: imie,
         role: klasa,
         level: 1,
+        skills: ["niegotujhani", "mega pierdnięcie"],
         equipment: {
             weapon: "miecz",
             armor: "złote gacie",
@@ -13,9 +14,32 @@ function createCharacter(imie, klasa) {
 }
 
 function addItem(character, item) {
-    
+    character.equipment.items.push(item);
 }
 
-const myCharacter = l
+function learnSkill(character, newSkill){
+    character.skills.push(newSkill);
+}
+
+function levelUp(character){
+    const currentLevel = character.level;
+    character.level = currentLevel + 1;
+}
+
+function characterDescription(character){
+    const description = character.name + "\n" + character.role + "\n" + character.skills;
+    console.log(description);
+}
+
+const myCharacter = createCharacter("Owad", "Pospolity");
+
+addItem(myCharacter, "buty");
+
+learnSkill(myCharacter, "Przywolywanie Tabubu");
+
+levelUp(myCharacter);
+
+characterDescription(myCharacter);
+
 
 console.log(myCharacter);
